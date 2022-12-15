1af16bf4d39854c6c2cbbf7dc15c2fdac25db93e
khazaaaar <zloiegor@mail.ru>
2022-12-15 16:29:56 +0200

    amazon

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,141 +1,130 @@
 import { HardhatUserConfig } from "hardhat/config";
-import "@nomiclabs/hardhat-ethers"
+import "@nomiclabs/hardhat-ethers";
 import "@nomicfoundation/hardhat-toolbox";
 import "hardhat-deploy-tenderly";
-import 'hardhat-deploy';
-import 'hardhat-deploy-ethers';
+import "hardhat-deploy";
+import "hardhat-deploy-ethers";
 import { task } from "hardhat/config";
-import '@nomiclabs/hardhat-ethers';
-import 'hardhat-contract-sizer';
-import '@nomiclabs/hardhat-etherscan'
+import "@nomiclabs/hardhat-ethers";
+import "hardhat-contract-sizer";
+import "@nomiclabs/hardhat-etherscan";
 
-
-const { mnemonic, bscscanApiKey } = require('./secrets.json');
+const { mnemonic, bscscanApiKey } = require("./secrets.json");
 
 //import "@tenderly/hardhat-tenderly";
 // tdly.setup({
 //   automaticVerifications: false
 // });
 
-task("accounts", "Prints the list of accounts")
-  .setAction(async (args, hre) => {
-    const tokenId = Number(args.id)
+task("accounts", "Prints the list of accounts").setAction(async (args, hre) => {
+    const tokenId = Number(args.id);
     const accounts = await hre.ethers.getSigners();
     for (const account of accounts) {
-      let bl = hre.ethers.utils.formatEther(await account.getBalance());
-      console.log(`${account.address}: ${bl}`);
+        let bl = hre.ethers.utils.formatEther(await account.getBalance());
+        console.log(`${account.address}: ${bl}`);
     }
-  });
+});
 
-task("verifys", "Verifys accounts")
-  .setAction(async (_args, { ethers, run }) => {
-    // await run("verify", {
-    //   address: "0xF8E133c6B4bC73d89723B138E0654AeaAD11Bd21",
-    //   contract: "contracts/ERC20Apple.sol:ERC20Apple",
-    // });
-
-    // await run("verify", {
-    //   address: "0x4075B23f1D93e99439eC078BB3E59712FE19B53A",
-    //   contract: "contracts/ERC20LSR.sol:ERC20LSR",
-    // });
-
-    // await run("verify", {
-    //   address: "0xac3849A6d4b0a97eC86998F6e0cC531D66F5Fa82",
-    //   contract: "contracts/ERC20Pancake.sol:ERC20Pancake",
-    // });
-
-    // await run("verify", {
-    //   address: "0xfA85901DBeB559EBA2d15bdc1c9EdfC14D880cAC",
-    //   contract: "contracts/ERC20Potato.sol:ERC20Potato",
-    // });
-
-    // await run("verify", {
-    //   address: "0xf1f8c1B19e56f34220B2eef5B19a15c2DF504f5F",
-    //   contract: "contracts/PancakeFactory.sol:PancakeFactory",
-    //   constructorArgsParams: [`${ethers.constants.AddressZero}`]
-    // });
-
-    // await run("verify", {
-    //   address: "0xEaa96e643d817D7DE691D8992d7535aaD458c6DD",
-    //   contract: "contracts/PancakePair.sol:PancakePair",
-    // });
-    // await run("verify", {
-    //   address: "0xB767f6d00424BDC3d148ad8ec7A882ef4BcEC1d3",
-    //   contract: "contracts/PancakeRouter_mod.sol:PancakeRouter_mod",
-    //   constructorArgsParams: [`0xf1f8c1B19e56f34220B2eef5B19a15c2DF504f5F`, `${ethers.constants.AddressZero}`, `0x4075B23f1D93e99439eC078BB3E59712FE19B53A`]
-    // });
-  });
+task("verifys", "Verifys accounts").setAction(
+    async (_args, { ethers, run }) => {
+        // await run("verify", {
+        //   address: "0xF8E133c6B4bC73d89723B138E0654AeaAD11Bd21",
+        //   contract: "contracts/ERC20Apple.sol:ERC20Apple",
+        // });
+        // await run("verify", {
+        //   address: "0x4075B23f1D93e99439eC078BB3E59712FE19B53A",
+        //   contract: "contracts/ERC20LSR.sol:ERC20LSR",
+        // });
+        // await run("verify", {
+        //   address: "0xac3849A6d4b0a97eC86998F6e0cC531D66F5Fa82",
+        //   contract: "contracts/ERC20Pancake.sol:ERC20Pancake",
+        // });
+        // await run("verify", {
+        //   address: "0xfA85901DBeB559EBA2d15bdc1c9EdfC14D880cAC",
+        //   contract: "contracts/ERC20Potato.sol:ERC20Potato",
+        // });
+        // await run("verify", {
+        //   address: "0xf1f8c1B19e56f34220B2eef5B19a15c2DF504f5F",
+        //   contract: "contracts/PancakeFactory.sol:PancakeFactory",
+        //   constructorArgsParams: [`${ethers.constants.AddressZero}`]
+        // });
+        // await run("verify", {
+        //   address: "0xEaa96e643d817D7DE691D8992d7535aaD458c6DD",
+        //   contract: "contracts/PancakePair.sol:PancakePair",
+        // });
+        // await run("verify", {
+        //   address: "0xB767f6d00424BDC3d148ad8ec7A882ef4BcEC1d3",
+        //   contract: "contracts/PancakeRouter_mod.sol:PancakeRouter_mod",
+        //   constructorArgsParams: [`0xf1f8c1B19e56f34220B2eef5B19a15c2DF504f5F`, `${ethers.constants.AddressZero}`, `0x4075B23f1D93e99439eC078BB3E59712FE19B53A`]
+        // });
+    }
+);
 
 task("testDEX", "Deploy, fund, add liq").setAction(
-  async (_args, { ethers, run }) => {
-    await run("run", { script: "scripts/fund-users.ts" });
-    await run("run", { script: "scripts/create-pairs.ts" });
-    await run("run", { script: "scripts/add-liquidityUNV.ts" });
-    await run("run", { script: "scripts/swapUNV.ts" });
-    await run("run", { script: "scripts/dex-status.ts" });
-
-  }
+    async (_args, { ethers, run }) => {
+        await run("run", { script: "scripts/fund-users.ts" });
+        await run("run", { script: "scripts/create-pairs.ts" });
+        await run("run", { script: "scripts/add-liquidityUNV.ts" });
+        await run("run", { script: "scripts/swapUNV.ts" });
+        await run("run", { script: "scripts/dex-status.ts" });
+    }
 );
 
 const config: HardhatUserConfig = {
-  //defaultNetwork: "localhost",
-  defaultNetwork: "hardhat",
-
-  networks: {
-    hardhat: {
-      allowUnlimitedContractSize: true,
-      blockGasLimit: 1245000000,
-      chainId: 31337,
-      loggingEnabled: false,
+    //defaultNetwork: "localhost",
+    defaultNetwork: "hardhat",
+    //chainId: 31337,
+    networks: {
+        hardhat: {
+            allowUnlimitedContractSize: true,
+            blockGasLimit: 1245000000,
+            chainId: 31337,
+            loggingEnabled: false,
+        },
+        local: {
+            url: "http://127.0.0.1:8545",
+            loggingEnabled: false,
+        },
+        testnet: {
+            url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+            chainId: 97,
+            gasPrice: 20000000000,
+            accounts: { mnemonic: mnemonic },
+            timeout: 1000000,
+        },
+        mainnet: {
+            url: "https://bsc-dataseed.binance.org/",
+            chainId: 56,
+            gasPrice: 20000000000,
+            accounts: { mnemonic: mnemonic },
+        },
     },
-    local: {
-      url: 'http://127.0.0.1:8545',
-      loggingEnabled: false,
-
+    solidity: {
+        compilers: [
+            { version: "0.5.16" },
+            { version: "0.6.6" },
+            { version: "0.8.0" },
+            { version: "0.8.4" },
+            { version: "0.4.18" },
+        ],
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: 10000,
+            },
+        },
     },
-    testnet: {
-      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
-      chainId: 97,
-      gasPrice: 20000000000,
-      accounts: { mnemonic: mnemonic },
-      timeout: 1000000
+    namedAccounts: {
+        deployer: 0,
     },
-    mainnet: {
-      url: "https://bsc-dataseed.binance.org/",
-      chainId: 56,
-      gasPrice: 20000000000,
-      accounts: { mnemonic: mnemonic }
-    }
 
-  },
-  solidity: {
-    compilers: [
-      { version: "0.5.16", },
-      { version: "0.6.6", },
-      { version: "0.8.0", },
-      { version: "0.8.4", },
-      { version: "0.4.18", },
-    ],
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 10000
-      }
-    }
-  },
-  namedAccounts: {
-    deployer: 0,
-  },
-
-  tenderly: {
-    project: 'PSRouter',
-    username: 'Khazaar',
-  },
-  etherscan: {
-    apiKey: bscscanApiKey
-  },
+    tenderly: {
+        project: "PSRouter",
+        username: "Khazaar",
+    },
+    etherscan: {
+        apiKey: bscscanApiKey,
+    },
 };
-
-
 
 export default config;
--- scripts/fund-users.ts
+++ scripts/fund-users.ts
@@ -1,52 +1,65 @@
 import { address } from "hardhat/internal/core/config/config-validation";
 //import { ERC20Apple__factory, ERC20Potato__factory } from "../typechain-types/"
-import { PancakeRouter_mod, ERC20Apple__factory, ERC20Potato__factory, ERC20LSR__factory } from "../typechain-types"
-import { ethers, deployments, getNamedAccounts } from 'hardhat';
-import 'hardhat-deploy';
-import 'hardhat-deploy-ethers';
+import {
+    PancakeRouter_mod,
+    ERC20Apple__factory,
+    ERC20Potato__factory,
+    ERC20LSR__factory,
+} from "../typechain-types";
+import { ethers, deployments, getNamedAccounts } from "hardhat";
+import "hardhat-deploy";
+import "hardhat-deploy-ethers";
 
 async function main() {
-  // Connetc to DEX
-  const [owner, user1, user2, user3, user4, user5] = await ethers.getSigners();
-  const users = [owner, user1, user2, user3, user4, user5];
+    // Connetc to DEX
+    const [owner, user1, user2, user3, user4, user5] =
+        await ethers.getSigners();
+    const users = [owner, user1, user2, user3, user4, user5];
 
-  const contractApple = await ethers.getContract('ERC20Apple');
-  const contractPotato = await ethers.getContract('ERC20Potato');
-  const contractLSR = await ethers.getContract('ERC20LSR');
-  const router_mod: PancakeRouter_mod = await ethers.getContract("PancakeRouter_mod");
+    const contractApple = await ethers.getContract("ERC20Apple");
+    const contractPotato = await ethers.getContract("ERC20Potato");
+    const contractLSR = await ethers.getContract("ERC20LSR");
+    const router_mod: PancakeRouter_mod = await ethers.getContract(
+        "PancakeRouter_mod"
+    );
+    //@todo
 
+    const appleAmount = BigInt(1000000);
+    const lsrAmount = BigInt(1000000);
+    const potatoAmount = BigInt(2000000);
 
-  const appleAmount = BigInt(1000000);
-  const lsrAmount = BigInt(1000000);
-  const potatoAmount = BigInt(2000000);
+    // Fund with Apple
+    await contractApple.connect(user1).getTokens(appleAmount.toString());
+    await contractApple.connect(user2).getTokens(appleAmount.toString());
+    await contractApple.connect(user3).getTokens(appleAmount.toString());
+    await contractApple.connect(owner).getTokens(appleAmount.toString());
 
-  // Fund with Apple
-  await contractApple.connect(user1).getTokens(appleAmount.toString());
-  await contractApple.connect(user2).getTokens(appleAmount.toString());
-  await contractApple.connect(user3).getTokens(appleAmount.toString());
-  await contractApple.connect(user4).getTokens(appleAmount.toString());
+    //Fund with Potato
+    //await contractPotato.connect(user2).getTokens(potatoAmount.toString());
+    await contractPotato.connect(user3).getTokens(appleAmount.toString());
+    //await contractPotato.connect(user4).getTokens(potatoAmount.toString());
 
-  //Fund with Potato
-  //await contractPotato.connect(user2).getTokens(potatoAmount.toString());
-  await contractPotato.connect(user3).getTokens(appleAmount.toString());
-  //await contractPotato.connect(user4).getTokens(potatoAmount.toString());
+    //Fund with LSR
+    await contractLSR.connect(owner).getTokens(lsrAmount.toString());
+    await contractLSR.connect(user3).getTokens(lsrAmount.toString());
+    console.log(`Funded successfully`);
 
-  //Fund with LSR
-  await contractLSR.connect(user4).getTokens(lsrAmount.toString());
-  await contractLSR.connect(user3).getTokens(lsrAmount.toString());
-  console.log(`Funded successfully`);
-
-
-  for (const usr of users) {
-    await contractApple.connect(usr).approve(router_mod.address, 9999999999999);
-    await contractPotato.connect(usr).approve(router_mod.address, 9999999999999);
-    await contractLSR.connect(usr).approve(router_mod.address, 9999999999999);
-  }
+    for (const usr of users) {
+        await contractApple
+            .connect(usr)
+            .approve(router_mod.address, 9999999999999);
+        await contractPotato
+            .connect(usr)
+            .approve(router_mod.address, 9999999999999);
+        await contractLSR
+            .connect(usr)
+            .approve(router_mod.address, 9999999999999);
+    }
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+    console.error(error);
+    process.exitCode = 1;
 });
